f3cac3ec6e6acd08f92ff326ba83a1d8ce1adedb
Marangely Rosas <Ladykaliber22@gmail.com>
2023-04-18 12:38:29 -0400

    Created models folder & transaction.js file

---
+++ controllers/transactionController.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const transactionController = express.Router();
+
+
+const transactionArray = require("../models/transactions.js");  
+
+transactionController.get("/", (req, res) => {    
+    res.send(transactionArray);    
+});
+
+transactionController.get('/:index', (req, res) => {
+    const { index } = req.params
+    if(transactionArray[index]) {
+    res.send(transactionArray[index]);
+  } else {
+    res.redirect("/404")
+  } 
+  })
+
+
+  transactionController.post('/', (req, res) => {
+    const transaction = {
+        transactionId: Math.floor(Math.random() * 999999),
+        item_name: req.body.item_name,
+        amount: req.body.amount,
+        date: req.body.date,
+        from: req.body.from,
+        category: req.body.category,
+      }
+    transactionArray.push(transaction)
+    // transArray.push( req.body);
+      res.json(transArray[transArray.length - 1]);
+  })
+
+transactionController.put('/:index', (req, res) => {
+    const { index } = req.params;
+    if(transactionArray[index]) {
+      transactionArray[index] = req.body;
+      res.status(200).json({ status: 200, message: "resource updated" });
+    } else {
+      res.redirect("/404")
+    }
+  })
+transactionController.delete("/:id", (req, res) => {
+    const {index} = req.params;
+    if(transactionArray[index]){
+        transactionArray.splice(index, 1);
+        res.status(200).json({ status: 200, message: "resource deleted" });
+    } else {
+      res.redirect("/404")
+    }
+});
+
+
+module.exports = transactionController;
---
+++ models/transactions.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+      transactionId: 0,
+      item_name: "Income",
+      name: 100,
+      date: "Apr-18-2023",
+      from: "Bank",
+      category: "Income"
+    },
+    {
+        transactionId: 1,
+        item_name: "cat food",
+        name: 200,
+        date: "Apr-18-2023",
+        from: "cat store",
+        category: "Income"
+    }
+]
